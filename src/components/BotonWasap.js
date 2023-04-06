7cdeab6c97d47852afcb984e55544d04705f9a8b
manuel pais <manupais22@gmail.com>
2023-04-05 21:24:04 -0300

    1.0

---
+++ src/components/BotonWasap.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { TouchableOpacity, StyleSheet, Text, Linking } from 'react-native';
+
+const whatsappLink = "https://wa.me/1165188743";
+
+function BotonFlotante() { 
+    return (    
+        <TouchableOpacity>
+            
+    <Text>
+        Hola
+    </Text>
+
+        </TouchableOpacity>)
+    }
+         
+        
+        
+        const styles = StyleSheet.create({ boton: { position: 'absolute', bottom: 20, right: 20, width: 60, height: 60, backgroundColor: '#25d366', borderRadius: 30, alignItems: 'center', justifyContent: 'center', elevation: 3, }, textoBoton: { fontSize: 12, color: '#fff', textAlign: 'center', }, }); 
+        
+        
+        
+        
+        
+        export default BotonFlotante;
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Paragraph from '../components/Paragraph'
+import Button from '../components/Button'
+import BotonWasap from '../components/BotonWasap'
+
+
+
+export default function Dashboard({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Let’s start</Header>
+      <Paragraph>
+        Your amazing app starts here. Open you favorite code editor and start
+        editing this project.
+      </Paragraph>
+      <BotonWasap>
+
+      </BotonWasap>
+      <Button
+        mode="outlined"
+        onPress={() =>
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'StartScreen' }],
+          })
+        }
+      >
+        Logout
+      </Button>
+    </Background>
+  )
+}
